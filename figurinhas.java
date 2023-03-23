825a24d39e4cc2b96c85bed40d5a7f98cc29a40a
JhullyVitoria <118947407+JhullyVitoria@users.noreply.github.com>
2023-03-23 12:11:40 -0300

    Add files via upload

---
+++ figurinhas.java
@@ -0,0 +1,12 @@
+package com.example.ex01;
+
+public class figurinhas
+{
+        String nome;
+        String nascimento;
+        float altura;
+        float peso;
+        String posicao;
+        String pais;
+
+}
